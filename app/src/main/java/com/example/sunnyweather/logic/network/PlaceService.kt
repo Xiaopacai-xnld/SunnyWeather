1a4c3e65274fcd9d174ebcdcc9a5fc3e970e5cbe
王浩 <haowang@tinnove.com.cn>
2026-01-19 15:22:20 +0800

    1.19

--- app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
+++ app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
@@ -2,13 +2,16 @@ package com.example.sunnyweather.logic.network
 
 import com.example.sunnyweather.SunnyWeatherApplication
 import com.example.sunnyweather.logic.model.PlaceResponse
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface PlaceService {
 
-    @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
-    fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+    @GET("v2/place")
+    suspend fun searchPlaces(
+        @Query("query") query: String,
+        @Query("token") token: String = SunnyWeatherApplication.TOKEN,
+        @Query("lang") lang: String = "zh_CN"
+    ): PlaceResponse
 
 }
